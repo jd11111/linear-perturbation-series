20ada7936fd2a8d1d0dd90337ac94be0ef46210f
jd11111 <jannik.daun@outlook.de>
2024-03-11 12:55:36 +0100

    modified:   app/Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,40 +1,42 @@
 module Main where
 
-
 import Numeric.LinearAlgebra
 import Combinatorics
 
-p:: Matrix R
-p = (3><3) [ 1, 2, 3, 4, 0, 5, 7, 7, 2]
-
-matMul :: Matrix R -> Matrix R -> Matrix R
+matMul :: Numeric t => Matrix t -> Matrix t -> Matrix t
 matMul = (Numeric.LinearAlgebra.<>)
 
-v :: Matrix R
-v = (3><3) [1,2,3,5,8,10,3,2,1]
-
-s :: Matrix R
-s = (3><3) [1,2,3,1,2,3,5,1,9]
-
-idm :: Matrix R
-idm = (3><3) [1,0,0,0,1,0,0,0,1]
-
-matPow :: Int -> Matrix R -> Matrix R
-matPow 0 _ = idm
+matPow ::Int -> Matrix C -> Matrix C
+matPow 0 _ = idMat
 matPow n m = matMul m (matPow (n-1) m)
 
-num2mat :: Int -> Matrix R
-num2mat 0 = v*p
-num2mat n = (-1)^(n+1)* matMul v (matPow n s)
-
-trace :: Matrix R -> R
+trace :: Matrix C -> C
 trace = sum . toList . takeDiag
 
-num2num :: Int -> R
-num2num = trace . num2mat
+p:: Matrix C
+p = (3><3) [ 1, 0, 0, 0, 0, 0, 0, 0, 0]
+
+v :: Matrix C
+v = (3><3) [1,-1,3,-5,3,2,-4,-8,1]
+
+s :: Matrix C
+s = (3><3) [0,0,0,0,1/(1-4),0,0,0,1/(1+3)]
+
+idMat :: Matrix C
+idMat = ident 3
+
+intToMat :: Int -> Matrix C
+intToMat 0 = matMul v p
+intToMat n = (-1)^(n+1)* matMul v (matPow n s)
+
+weakCombToMat :: [Int] -> Matrix C
+weakCombToMat [] = idMat
+weakCombToMat (x:xs) = matMul (intToMat x) (weakCombToMat xs)
+
+pertCoeff :: Int -> C
+pertCoeff n = trace . sum $ (map weakCombToMat (weakComps (n+1)))
 
 main:: IO()
 main = do{
-    print $ num2num 5;
-    print $ weakComps 4;
+  print $ map pertCoeff (take 13 [0..]);
 }
