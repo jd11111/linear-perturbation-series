23e63f1f6f7e1cd773679312028c5a74c7876c4b
jd11111 <jannik.daun@outlook.de>
2024-03-13 11:59:31 +0100

    new file:   cabal.project.local

--- app/Main.hs
+++ app/Main.hs
@@ -5,11 +5,8 @@ import Data.Array
 import Data.List
 import Control.Parallel.Strategies
 
-goodSum:: Num d => [d] -> d
-goodSum = foldl' (+) 0
-
 matMul :: (Ix a, Ix b, Ix c, Num d) => Array (a,b) d -> Array (b,c) d -> Array (a,c) d
-matMul x y = array resultBounds [((i,j), goodSum [x!(i,k) * y!(k,j) | k <- range (lj,uj)])| i <- range (li,ui), j <- range (lj',uj')]
+matMul x y = array resultBounds [((i,j), sum [x!(i,k) * y!(k,j) | k <- range (lj,uj)])| i <- range (li,ui), j <- range (lj',uj')]
         where ((li,lj),(ui,uj))         =  bounds x
               ((li',lj'),(ui',uj'))     =  bounds y
               resultBounds | (lj,uj)==(li',ui')    =  ((li,lj'),(ui,uj')) | otherwise = error "wrong shape"
@@ -17,9 +14,12 @@ matMul x y = array resultBounds [((i,j), goodSum [x!(i,k) * y!(k,j) | k <- range
 trace :: (Ix a, Num d) => Array (a,a) d -> d
 trace x = sum [x!(i,i) | i <- range(li,ui)] where ((li,_),(ui,_)) = bounds x 
 
-matPow :: (Ix a, Num d) => Int -> Array (a,a) d -> Array (a,a) d
+matPow :: (Ix a, Num d) => Int -> Array (a,a) d -> Array (a,a) d --memoize this
 matPow 1 x = x
 matPow n x = matMul x (matPow (n-1) x)
+
+memoMatPow :: (Ix b, Num d) => Array (b, b) d -> Int -> Array (b, b) d
+memoMatPow x = (map (pow x) [0 ..] !!) where { pow x 1 = x; pow x n = matMul x (memoMatPow x (n-1))}
 
 scaMul :: (Ix a, Num d) => d -> Array (a,a) d -> Array (a,a) d
 scaMul alpha x = array ((li,lj),(ui,uj)) [((i,j),alpha*x!(i,j)) | i <- range (li,ui), j <- range (lj,uj)] where ((li,lj),(ui,uj)) = bounds x
@@ -34,18 +34,21 @@ v = array ((0,0),(2,2)) [((0,0),1.0), ((0,1),-1.0),((0,2),3.0), ((1,0),-5.0), ((
 s:: Array (Int, Int) Double
 s = array ((0,0),(2,2)) [((0,0),0.0), ((0,1),0.0),((0,2),0.0), ((1,0),0.0), ((1,1),1.0/(1.0-4.0)), ((1,2),0.0), ((2,0),0.0), ((2,1),0.0), ((2,2),1.0/(1.0+3.0))]
 
+sMemo = memoMatPow s
 
-intToMat :: Int -> Array (Int, Int) Double
+intToMat :: Int -> Array (Int, Int) Double --memoize this 
 intToMat 0 = matMul v p
-intToMat n = scaMul ((-1.0)^(n+1)) (matMul v (matPow n s))
+intToMat n = scaMul ((-1.0)^(n+1)) (matMul v (sMemo n))
 
-weakCombToMat :: [Int] -> Array (Int, Int) Double
+memoIntToMat = (map intToMat [0 ..] !!)
+
+weakCombToMat :: [Int] -> Array (Int, Int) Double --can i memoize this too?
 weakCombToMat (x:[])= intToMat x
-weakCombToMat (x:xs) = matMul (intToMat x) (weakCombToMat xs)
+weakCombToMat (x:xs) = matMul (memoIntToMat x) (weakCombToMat xs)
 
-pertCoeff n = goodSum $ parMap rdeepseq (trace . weakCombToMat) (weakComps (n+1))
+pertCoeff n = sum $ map (trace . weakCombToMat) (weakComps (n+1))
 
 main:: IO()
 main = do{
-  print $ map pertCoeff (take 14 [0..]);
+  print $ parMap rdeepseq pertCoeff (take 14 [0..]);
 }
