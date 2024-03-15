b79edf0ebc140f896d9a18f3c1c72750f91d5e77
jd11111 <jannik.daun@outlook.de>
2024-03-15 10:44:08 +0100

    modified:   app/Main.hs 	modified:   linear-perturbation-series.cabal

--- app/Main.hs
+++ app/Main.hs
@@ -2,24 +2,22 @@ module Main where
 
 import Combinatorics
 import Data.Array
-import Data.List
-import Control.Parallel.Strategies
-
 
 import qualified Control.Monad.State.Strict as CMSS
 import qualified Data.HashMap.Strict as DHS
 import qualified Data.Hashable
 
 matMul :: (Ix a, Ix b, Ix c, Num d) => Array (a,b) d -> Array (b,c) d -> Array (a,c) d
-matMul x y = array resultBounds [((i,j), sum [x!(i,k) * y!(k,j) | k <- range (lj,uj)])| i <- range (li,ui), j <- range (lj',uj')]
-        where ((li,lj),(ui,uj))         =  bounds x
-              ((li',lj'),(ui',uj'))     =  bounds y
-              resultBounds | (lj,uj)==(li',ui')    =  ((li,lj'),(ui,uj')) | otherwise = error "wrong shape"
+matMul x y = array resultBounds [((i,j), sum [x!(i,k) * y!(k,j) | k <- range (lk,uk)])| i <- range (li,ui), j <- range (lj,uj)] where{
+  ((li,lk),(ui,uk)) = bounds x;
+  ((lm,lj),(um,uj)) = bounds y;
+  resultBounds | (lk,uk)==(lm,um) = ((li,lj),(ui,uj)) | otherwise = error "wrong shape"
+  }
 
 trace :: (Ix a, Num d) => Array (a,a) d -> d
 trace x = sum [x!(i,i) | i <- range(li,ui)] where ((li,_),(ui,_)) = bounds x 
 
-matPow :: (Ix a, Num d) => Int -> Array (a,a) d -> Array (a,a) d --memoize this
+matPow :: (Ix a, Num d) => Int -> Array (a,a) d -> Array (a,a) d
 matPow 1 x = x
 matPow n x = matMul x (matPow (n-1) x)
 
@@ -32,25 +30,24 @@ scaMul alpha x = array ((li,lj),(ui,uj)) [((i,j),alpha*x!(i,j)) | i <- range (li
 matSum :: (Ix a, Num d) => Array (a,a) d -> Array (a,a) d -> Array (a,a) d
 matSum x y = array ((li,lj),(ui,uj)) [((i,j),y!(i,j) + x!(i,j)) | i <- range (li,ui), j <- range (lj,uj)] where ((li,lj),(ui,uj)) = bounds x
 
-p:: Array (Int, Int) Double
+p:: Array (Int, Int) Float
 p = array ((0,0),(2,2)) [((0,0),1.0), ((0,1),0.0),((0,2),0.0), ((1,0),0.0), ((1,1),0.0), ((1,2),0.0), ((2,0),0.0), ((2,1),0.0), ((2,2),0.0)]
-v:: Array (Int, Int) Double
+v:: Array (Int, Int) Float
 v = array ((0,0),(2,2)) [((0,0),1.0), ((0,1),-1.0),((0,2),3.0), ((1,0),-5.0), ((1,1),3.0), ((1,2),2.0), ((2,0),-4.0), ((2,1),-8.0), ((2,2),1.0)]
-s:: Array (Int, Int) Double
+s:: Array (Int, Int) Float
 s = array ((0,0),(2,2)) [((0,0),0.0), ((0,1),0.0),((0,2),0.0), ((1,0),0.0), ((1,1),1.0/(1.0-4.0)), ((1,2),0.0), ((2,0),0.0), ((2,1),0.0), ((2,2),1.0/(1.0+3.0))]
 
-type MyMemo a b = CMSS.State (DHS.HashMap a b) b
-
-
-memoIntToMat :: Int -> Array (Int, Int) Double
+memoIntToMat :: Int -> Array (Int, Int) Float
 memoIntToMat = (map intToMat [0 ..] !!)
 
-sMemo :: Int -> Array (Int, Int) Double
+sMemo :: Int -> Array (Int, Int) Float
 sMemo = memoMatPow s
 
-intToMat :: Int -> Array (Int, Int) Double 
+intToMat :: Int -> Array (Int, Int) Float 
 intToMat 0 = matMul v p
 intToMat n = scaMul ((-1.0)^(n+1)) (matMul v (sMemo n))
+
+type MyMemo a b = CMSS.State (DHS.HashMap a b) b
 
 myMemo :: Data.Hashable.Hashable a => (a -> MyMemo a b) -> a -> MyMemo a b
 myMemo f x = CMSS.gets (DHS.lookup x) >>= maybe z return where z = f x >>= (\ y -> CMSS.modify (DHS.insert x y) >> return y)
@@ -58,16 +55,15 @@ myMemo f x = CMSS.gets (DHS.lookup x) >>= maybe z return where z = f x >>= (\ y 
 runMyMemo :: (t -> CMSS.State s a) -> s -> t -> (a, s)
 runMyMemo f sta x = CMSS.runState (f x) sta 
 
-weakCombToMatMemo :: [Int] -> MyMemo [Int] (Array (Int, Int) Double)
+weakCombToMatMemo :: [Int] -> MyMemo [Int] (Array (Int, Int) Float)
 weakCombToMatMemo (x:[])= return (memoIntToMat x)
 weakCombToMatMemo (x:xs) = myMemo weakCombToMatMemo xs >>= \y -> return $ matMul (memoIntToMat x) y
 
-pertCoeffNew :: [[Int]] -> (Double, DHS.HashMap [Int] (Array (Int, Int) Double))
+pertCoeffNew :: [[Int]] -> (Float, DHS.HashMap [Int] (Array (Int, Int) Float))
 pertCoeffNew [] = (0.0,DHS.empty)
-pertCoeffNew (x:xs) = (a,b) where{ a = fst (pertCoeffNew xs) + (trace . fst . runMyMemo weakCombToMatMemo (snd (pertCoeffNew xs))) x; b = snd . runMyMemo weakCombToMatMemo (snd (pertCoeffNew xs)) $ x;}
+pertCoeffNew (x:xs) = let w = pertCoeffNew xs in let z = runMyMemo weakCombToMatMemo (snd w) in (fst w + (trace . fst . z) x,snd . z $ x)
 
 main:: IO()
 main = do{
-  --print $ fst ( pertCoeffNew (weakComps 9));
-  print $ map (fst . pertCoeffNew . weakComps) (take 11 [0..]);
+  print $ map (fst . pertCoeffNew . weakComps) (take 14 [1..]);
 }
