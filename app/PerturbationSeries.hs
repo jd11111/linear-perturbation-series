722930a4cdb3bccecb3587ae40657d933058c921
jd11111 <jannik.daun@outlook.de>
2024-03-19 15:17:08 +0100

    modified:   app/Main.hs 	new file:   app/PerturbationSeries.hs 	modified:   app/test.hs 	modified:   doc/doc.pdf 	modified:   doc/doc.tex 	modified:   linear-perturbation-series.cabal

---
+++ app/PerturbationSeries.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE BangPatterns #-}
+module PerturbationSeries where
+    
+import Combinatorics
+import qualified Control.Monad.State.Strict as CMSS
+import qualified Data.HashMap.Strict as DHS
+import qualified Data.Hashable
+import Data.Array.Unboxed
+import MatMul
+
+trace :: Matrix -> Float
+trace x =let{
+    a = int_data x;
+    ca = cols x;
+} in sum [a!(i*ca + i)| i <- range(0,ca-1)]
+
+matPow ::  Int -> Matrix -> Matrix
+matPow 1 x = x
+matPow n x = matMul x (matPow (n-1) x)
+
+memoMatPow :: Matrix -> Int -> Matrix
+memoMatPow x = (map (pow x) [0 ..] !!) where { pow y 1 = y; pow y n = matMul y (memoMatPow y (n-1))}
+
+scaMul :: Float -> Matrix -> Matrix
+scaMul alpha x = let{a = int_data x; ra = rows x; ca = cols x;} in let{!z = array (li,ui) [(i,alpha*(a! i)) | i <- range (li, ui)] where (li,ui)=bounds a} in GenMat{int_data=z, rows = ra, cols = ca}
+
+intToMat:: Matrix -> Matrix -> Matrix -> Int -> Matrix
+intToMat v p _ 0 = matMul v p
+intToMat v _ s n = scaMul ((-1.0)^(n+1)) (matMul v (memoMatPow s n))
+
+memoIntToMat :: Matrix -> Matrix -> Matrix -> Int -> Matrix
+memoIntToMat v p s  = ((map (intToMat v p s) [0 ..]) !!)
+
+type MyMemo a b = CMSS.State (DHS.HashMap a b) b
+
+myMemo :: Data.Hashable.Hashable a => (a -> MyMemo a b) -> a -> MyMemo a b
+myMemo f x = CMSS.gets (DHS.lookup x) >>= maybe z return where z = f x >>= (\ y -> CMSS.modify (DHS.insert x y) >> return y)
+
+runMyMemo :: (t -> CMSS.State s a) -> s -> t -> (a, s)
+runMyMemo f sta x = CMSS.runState (f x) sta
+
+weakCombToMatMemo :: Matrix -> Matrix -> Matrix -> [Int] -> MyMemo [Int] Matrix
+weakCombToMatMemo v p s [x] = return (memoIntToMat v p s x)
+weakCombToMatMemo v p s (x:xs) = myMemo (weakCombToMatMemo v p s) xs >>= \y -> return $ matMul (memoIntToMat v p s x) y
+weakCombToMatMemo v p s _ = undefined
+
+pertCoeffNew2 :: Matrix -> Matrix -> Matrix ->  DHS.HashMap [Int] Matrix-> [[Int]] -> (Float, DHS.HashMap [Int] Matrix)
+pertCoeffNew2 v p s st [] = (0.0,st)
+pertCoeffNew2 v p s st (x:xs) = let w = pertCoeffNew2 v p s st xs in let z = runMyMemo (weakCombToMatMemo v p s) (snd w) in (fst w + (trace . fst . z) x,snd . z $ x)
+
+pertCoeff :: Matrix -> Matrix -> Matrix -> Int -> ([Float],DHS.HashMap [Int] Matrix)
+pertCoeff v p s 1 = let z = pertCoeffNew2 v p s DHS.empty (weakComps 1) in ([fst z],snd z)
+pertCoeff v p s n = let w = pertCoeff v p s (n-1) in let z = pertCoeffNew2 v p s (snd w) (weakComps n) in ((fst z):(fst w),snd z)
--- app/test.hs
+++ app/test.hs
@@ -1,7 +1,60 @@
+import System.Random
+import MatMul
+import PerturbationSeries
+import Data.Array.Unboxed
 
---exactPertCoeffs n 
+fac :: Integer -> Integer --factorial function
+fac 0 = 1
+fac 1 = 1
+fac n = n * fac (n-1)
+
+falling:: Float -> Integer -> Float --"poch-hammer symbol"
+falling _ 0 = 1
+falling r k = r * falling (r-1) (k-1)
+
+binomCoeff :: Float -> Integer -> Float -- generalized binomial coefficient
+binomCoeff r k = falling r k / fromInteger (fac k)
+
+--exact solution for the coefficients in the perturbation series for
+-- the eigenvalue that splits from e1:
+pertCoeffExactE1 ::  Float -> Float -> Float -> Integer -> Float 
+pertCoeffExactE1 e1 e2 a k = 0.5*(2.0*a)^(2*k)* binomCoeff 0.5 k /((e1 - e2)^(2*k-1))
+
+--exact solution for the coefficients in the perturbation series for
+--the eigenvalue that splits from e2:
+pertCoeffExactE1 e1 e2 a k = 0.5*(2.0*a)^(2*k)* binomCoeff 0.5 k /((e1 - e2)^(2*k-1))
+pertCoeffExactE2 ::  Float -> Float -> Float -> Integer -> Float 
+pertCoeffExactE2 e1 e2 a k = (-1)*pertCoeffExactE1 e1 e2 a k
+
+n :: Integer
+n = 5 
+
+closeCheck :: Float -> Float -> Bool
+closeCheck f1 f2 = abs (f1 - f2)  <= 0.000001* (abs f1 + abs f2)
+
+allClose :: [Float] -> [Float] -> Bool
+allClose l1 l2 = foldr (&&) True (map (uncurry closeCheck) (zip l1 l2))
 
 main :: IO()
 main = do{
-    print $ "hello world";
-}
+    gen <- newStdGen;
+    let ns = randoms gen :: [Float] in let {
+        e1 = 2.0*(ns !! 1)-1.0;
+        e2 = 2.0*(ns !! 2)-1.0;
+        a = 2.0*(ns !! 3) -1.0;
+        p1 = GenMat{int_data = listArray (0,3) [1.0,0.0,0.0,0.0], rows=2, cols=2};
+        p2 = GenMat{int_data = listArray (0,3) [0.0,0.0,0.0,1.0], rows=2, cols=2};
+        v = GenMat{int_data = listArray (0,3) [0.0,a,a,0.0], rows=2, cols=2};
+        s1 = GenMat{int_data = listArray (0,3) [0.0,0.0,0.0,1/(e1 -e2)], rows=2, cols=2};
+        s2 = GenMat{int_data = listArray (0,3) [1/(e2-e1),0.0,0.0,0.0], rows=2, cols=2};
+        x = concatMap (\z-> 0:[z]) $ map (pertCoeffExactE1 e1 e2 a) [1.. n];
+        y = concatMap (\z-> 0:[z]) $ map (pertCoeffExactE2 e1 e2 a) [1..n];
+        z1 = reverse $ fst (pertCoeff v p1 s1 (2* fromInteger n));
+        z2 = reverse $ fst (pertCoeff v p2 s2 (2* fromInteger n));
+        } in (print "exact E1:") >> (print x)
+        >> (print "calculated:") >> (print z1)
+        >> (print "all close?") >> (print $ allClose x z1)
+        >> (print "exact E2") >> (print y)
+        >> (print "calculated:") >> (print z2)
+        >> (print "all close?") >> (print $ allClose y z2 )
+    }
