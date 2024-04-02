4d2a9e9516efeb863ba835d0d07c4d065df4da7a
jd11111 <jannik.daun@outlook.de>
2024-04-02 13:55:19 +0200

    updated memo

--- app/PerturbationSeries.hs
+++ app/PerturbationSeries.hs
@@ -19,21 +19,12 @@ matPow :: Int -> Matrix -> Matrix
 matPow 1 x = x
 matPow n x = matMul x (matPow (n - 1) x)
 
-memoMatPow :: Matrix -> Int -> Matrix
-memoMatPow x = (map (pow x) [0 ..] !!) where pow y 1 = y; pow y n = matMul y (memoMatPow y (n - 1))
-
 scaMul :: Float -> Matrix -> Matrix
 scaMul alpha x = let a = int_data x; ra = rows x; ca = cols x in let !z = array (li, ui) [(i, alpha * (a ! i)) | i <- range (li, ui)] where (li, ui) = bounds a in GenMat {int_data = z, rows = ra, cols = ca}
 
 intToMat :: Matrix -> Matrix -> Matrix -> Int -> Matrix
 intToMat v p _ 0 = matMul v p
 intToMat v _ s n = scaMul ((-1.0) ^ (n + 1)) (matMul v (matPow n s)) -- old: memoMatPow
-
--- memoIntToMat :: Matrix -> Matrix -> Matrix -> Int -> Matrix
--- memoIntToMat v p s = (map (intToMat v p s) [0 ..] !!)
-
-memIntToMat :: Matrix -> Matrix -> Matrix -> [Matrix]
-memIntToMat v p s = map (intToMat v p s) [0 ..]
 
 type MyMemo a b = CMSS.State (DHS.HashMap a b) b
 
@@ -43,15 +34,18 @@ myMemo f x = CMSS.gets (DHS.lookup x) >>= maybe z return where z = f x >>= (\y -
 runMyMemo :: (t -> CMSS.State s a) -> s -> t -> (a, s)
 runMyMemo f sta x = CMSS.runState (f x) sta
 
-weakCombToMatMemo :: Matrix -> Matrix -> Matrix -> [Int] -> MyMemo [Int] Matrix
-weakCombToMatMemo v p s [x] = return (intToMat v p s x) -- was memoIntToMat
-weakCombToMatMemo v p s (x : xs) = myMemo (weakCombToMatMemo v p s) xs >>= \y -> return $ matMul (intToMat v p s x) y
-weakCombToMatMemo _ _ _ _ = undefined
+weakCombToMatMemo :: [Matrix] -> [Int] -> MyMemo [Int] Matrix
+weakCombToMatMemo mem [x] = return (mem !! x) -- was memoIntToMat
+weakCombToMatMemo mem (x : xs) = myMemo (weakCombToMatMemo mem) xs >>= \y -> return $ matMul (mem !! x) y
+weakCombToMatMemo _ _ = undefined
 
-pertCoeffNew2 :: Matrix -> Matrix -> Matrix -> DHS.HashMap [Int] Matrix -> [[Int]] -> (Float, DHS.HashMap [Int] Matrix)
-pertCoeffNew2 _ _ _ st [] = (0.0, st)
-pertCoeffNew2 v p s st (x : xs) = let w = pertCoeffNew2 v p s st xs in let z = runMyMemo (weakCombToMatMemo v p s) (snd w) in (fst w + (trace . fst . z) x, snd . z $ x)
+pertCoeffNew2 :: [Matrix] -> DHS.HashMap [Int] Matrix -> [[Int]] -> (Float, DHS.HashMap [Int] Matrix)
+pertCoeffNew2 _ st [] = (0.0, st)
+pertCoeffNew2 mem st (x : xs) = let w = pertCoeffNew2 mem st xs in let z = runMyMemo (weakCombToMatMemo mem) (snd w) in (fst w + (trace . fst . z) x, snd . z $ x)
 
-pertCoeff :: Matrix -> Matrix -> Matrix -> Int -> ([Float], DHS.HashMap [Int] Matrix) -- v is the perturbation, p the projection onto the eigenspace under consideration and s the reduced resolvent
-pertCoeff v p s 1 = let z = pertCoeffNew2 v p s DHS.empty (weakComps 1) in ([fst z], snd z)
-pertCoeff v p s n = let w = pertCoeff v p s (n - 1) in let z = pertCoeffNew2 v p s (snd w) (weakComps n) in ((fst z) : (fst w), snd z)
+memPertCoeff :: [Matrix] -> Int -> ([Float], DHS.HashMap [Int] Matrix) -- v is the perturbation, p the projection onto the eigenspace under consideration and s the reduced resolvent
+memPertCoeff mem 1 = let z = pertCoeffNew2 mem DHS.empty (weakComps 1) in ([fst z], snd z)
+memPertCoeff mem n = let w = memPertCoeff mem (n - 1) in let z = pertCoeffNew2 mem (snd w) (weakComps n) in (fst z : fst w, snd z)
+
+pertCoeff :: Matrix -> Matrix -> Matrix -> Int -> ([Float], DHS.HashMap [Int] Matrix)
+pertCoeff v p s n = let mem = map (intToMat v p s) [0 ..] in memPertCoeff mem n
